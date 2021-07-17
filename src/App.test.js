72d28bd06c850bd0c6fd0b260e5f493e0571b6bb
wtoldt <wes.oldt@gmail.com>
2021-07-17 12:56:14 -0700

    break test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/pig game/i);
+  const linkElement = screen.getByText(/forget react/i);
   expect(linkElement).toBeInTheDocument();
 });
