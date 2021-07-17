2d3ddbb059253175755ba13d321e9cdfe5caa178
wtoldt <wes.oldt@gmail.com>
2021-07-16 23:35:31 -0700

    add pig

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import pig from './assets/sow_pig.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pig} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
