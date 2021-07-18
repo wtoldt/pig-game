4b2bbd77adccc44d9116ceb0305981afa6975d9a
wtoldt <wes.oldt@gmail.com>
2021-07-17 22:08:41 -0700

    first attempt at game rules

--- src/App.consts.js
+++ src/App.consts.js
@@ -2,3 +2,14 @@ export const PLAYER_STATE = {
   overallScore: 0,
   currentScore: 0
 };
+
+export const INITIAL_STATE = {
+  p1: {name: 'P1', ...PLAYER_STATE},
+  p2: {name: 'P2', ...PLAYER_STATE},
+  currentPlayerId: 'p1',
+  dieValue: 1,
+  winner: undefined
+};
+
+export const WIN_THRESHOLD = 28;
+export const LOSER_NUMBER = 1;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React, { Component } from 'react';
 import Board from './components/Board/Board';
 import PlayerPanel from './components/PlayerPanel/PlayerPanel';
-import { PLAYER_STATE } from './App.consts';
+import GameControls from './components/GameControls/GameControls';
+import { INITIAL_STATE, WIN_THRESHOLD, LOSER_NUMBER } from './App.consts';
 
 import './App.scss';
 
 export default class App extends Component {
 
-  state = {
-    p1: {name: 'P1', ...PLAYER_STATE},
-    p2: {name: 'P2', ...PLAYER_STATE},
-    currentPlayer: 'P1',
-    winner: undefined
-  };
+  state = {...INITIAL_STATE};
+
+  handleOnGameReset() {
+    this.setState({...INITIAL_STATE});
+  }
+
+  /**
+   * @description determins the current player and returns a shallow copy.
+   * Good for manipulating the current player in place, but not modifying the state.
+   * @returns a copy of the current player
+  */
+  getCurrentPlayer() {
+    const { p1, p2, currentPlayerId } = this.state;
+    return currentPlayerId === 'p1' ? {...p1} : {...p2};
+  }
+
+  handleOnRoll() {
+    const { p1, p2, currentPlayerId } = this.state;
+    const currentPlayer = this.getCurrentPlayer();
+
+    const dieValue = Math.floor(Math.random() * 6) + 1;
+    if (dieValue === LOSER_NUMBER) {
+      //the player loses their current score, and next player takes a turn
+      currentPlayer.currentScore = 0;
+      this.setState({
+        dieValue,
+        p1: currentPlayerId === 'p1' ? currentPlayer : p1,
+        p2: currentPlayerId === 'p2' ? currentPlayer : p2,
+        currentPlayerId: currentPlayerId === 'p1' ? 'p2' : 'p1'
+      });
+    } else {
+      currentPlayer.currentScore += dieValue;
+      const total = currentPlayer.currentScore + currentPlayer.overallScore;
+      if (total >= WIN_THRESHOLD) {
+        //player has won, game is over!
+        this.setState({
+          winner: currentPlayer,
+          p1: currentPlayerId === 'p1' ? currentPlayer : p1,
+          p2: currentPlayerId === 'p2' ? currentPlayer : p2,
+          dieValue
+        });
+        alert(`${currentPlayer.name} has won with total score of ${total}! (${WIN_THRESHOLD} to win)`);
+      } else {
+        this.setState({
+          p1: currentPlayerId === 'p1' ? currentPlayer : p1,
+          p2: currentPlayerId === 'p2' ? currentPlayer : p2,
+          dieValue
+        });
+      }
+    }
+  }
+
+  handleOnHold() {
+    const { p1, p2, currentPlayerId } = this.state;
+    const currentPlayer = this.getCurrentPlayer();
+
+    //add currentScore to overallScore and change currentPlayer
+    currentPlayer.overallScore += currentPlayer.currentScore;
+    currentPlayer.currentScore = 0;
+    this.setState({
+      p1: currentPlayerId === 'p1' ? currentPlayer : p1,
+      p2: currentPlayerId === 'p2' ? currentPlayer : p2,
+      currentPlayerId: currentPlayerId === 'p1' ? 'p2' : 'p1'
+    });
+  }
 
   render() {
-    const { p1, p2, currentPlayer } = this.state;
+    const { p1, p2, currentPlayerId, dieValue, winner } = this.state;
     return (
       <div className="pg-app">
         <Board>
-          <PlayerPanel player={p1} active={currentPlayer === p1.name} left />
-          <PlayerPanel player={p2} active={currentPlayer === p2.name} right />
+          <PlayerPanel player={p1} active={currentPlayerId === 'p1'} left />
+          <GameControls dieValue={dieValue} disabled={winner}
+            onGameReset={this.handleOnGameReset.bind(this)}
+            onRoll={this.handleOnRoll.bind(this)}
+            onHold={this.handleOnHold.bind(this)}
+          />
+          <PlayerPanel player={p2} active={currentPlayerId === 'p2'} right />
         </Board>
       </div>
     );
